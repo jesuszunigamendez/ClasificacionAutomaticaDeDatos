18728b0efedbcadbbd1510cb598de2d26bc78bbd
jesuszunigamendez <134381269+jesuszunigamendez@users.noreply.github.com>
2024-12-01 23:17:26 -0600

    codigo revisado

--- ProyectoClasificacionAutomatica/scripts/entrenamiento.py
+++ ProyectoClasificacionAutomatica/scripts/entrenamiento.py
@@ -247,8 +247,8 @@ class Modelos:
             MaxPooling2D((2, 2), strides=(2, 2)),
 
             # Bloque 4
-            #Conv2D(256, (3, 3), activation='relu', padding='same'),
-            #MaxPooling2D((2, 2), strides=(2, 2)),
+            Conv2D(256, (3, 3), activation='relu', padding='same'),
+            MaxPooling2D((2, 2), strides=(2, 2)),
 
 
             # Clasificación
